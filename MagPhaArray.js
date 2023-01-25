dd3f127acc4d4f3629fcd9ef0d87b8c31232924c
sefirek <sefir@o2.pl>
2023-01-25 11:12:46 +0100

    Usunięcie warunku sprawdzania typu ComplexArray

--- MagPhaArray.js
+++ MagPhaArray.js
@@ -1,39 +1,34 @@
-import ComplexArray from './ComplexArray.js';
 import MagPha from './MagPha.js';
 
 export default class MagPhaArray extends Array {
-	constructor(complexArray) {
-		super();
-		if (!(complexArray instanceof ComplexArray))
-			throw new Error(
-				'Wrong type of a complexArray argument, expected ComplexArray type'
-			);
-		this.complexArray = complexArray.deepCopy();
-		for (let i = 0; i < this.complexArray.length / 2; i += 1) {
-			super.push(new MagPha(i, this.complexArray));
-		}
-	}
+  constructor(complexArray) {
+    super();
+    this.complexArray = complexArray.deepCopy();
+    for (let i = 0; i < this.complexArray.length / 2; i += 1) {
+      super.push(new MagPha(i, this.complexArray));
+    }
+  }
 
-	slice(start, end) {
-		return [...this].slice(start, end);
-	}
+  slice(start, end) {
+    return [...this].slice(start, end);
+  }
 
-	push() {
-		return this;
-	}
+  push() {
+    return this;
+  }
 
-	getPreparedComplexArray() {
-		return this.complexArray;
-	}
+  getPreparedComplexArray() {
+    return this.complexArray;
+  }
 
-	deleteFrequencies(...frequencies) {
-		frequencies.forEach((frequency) => this[frequency]?.zero());
-	}
+  deleteFrequencies(...frequencies) {
+    frequencies.forEach((frequency) => this[frequency]?.zero());
+  }
 
-	deleteAllOthersFrequencies(...frequencies) {
-		const allFrequencies = {};
-		for (let i = 0; i < this.length; i += 1) allFrequencies[i] = i;
-		frequencies.forEach((frequency) => delete allFrequencies[frequency]);
-		Object.keys(allFrequencies).forEach((frequency) => this[frequency]?.zero());
-	}
+  deleteAllOthersFrequencies(...frequencies) {
+    const allFrequencies = {};
+    for (let i = 0; i < this.length; i += 1) allFrequencies[i] = i;
+    frequencies.forEach((frequency) => delete allFrequencies[frequency]);
+    Object.keys(allFrequencies).forEach((frequency) => this[frequency]?.zero());
+  }
 }
